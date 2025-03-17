d9874723ad3431d1a73d9f4b50ff355eb8f3431f
Petras Vestartas <petrasvestartas@gmail.com>
2025-03-17 17:59:51 +0100

    v34

--- src/test_my_binding/__init__.py
+++ src/test_my_binding/__init__.py
@@ -5,7 +5,7 @@ __author__ = ["tom van mele", "petras vestartas"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = ["van.mele@arch.ethz.ch", "vestartas@arch.ethz.ch"]
-__version__ = "1.0.33"
+__version__ = "1.0.34"
 
 HERE = os.path.dirname(__file__)
 
