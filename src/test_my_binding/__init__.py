34bcc6d9280204a62671b1274b9f230d1f4b4394
petrasvestartas <petrasvestartas@gmail.com>
2025-03-24 20:52:02 +0100

    Bump version to 1.0.88

--- src/test_my_binding/__init__.py
+++ src/test_my_binding/__init__.py
@@ -5,7 +5,7 @@ __author__ = ["tom van mele", "petras vestartas"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = ["van.mele@arch.ethz.ch", "vestartas@arch.ethz.ch"]
-__version__ = "1.0.87"
+__version__ = "1.0.88"
 
 HERE = os.path.dirname(__file__)
 
